a23d184031e2ccbd7bd35ab925ac49a5d11fe792
TheRealNapla <69878881+TheRealNapla@users.noreply.github.com>
2021-12-01 19:26:24 -0500

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,14 @@
+def sortName(val):
+  return val[1]
+
+def sortBrand(val):
+  return val[2]
+
+def sortType(val):
+  return val[3]
+
+def sortQuantity(val):
+  return val[5]
+
+def sortID(val):
+  return val[0]
